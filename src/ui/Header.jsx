f5354f88c58aee082dba67d2e29c86f49a28a3f4
jamesAkash <akash.james@outlook.com>
2023-11-01 17:59:17 +0530

    first commit

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Link to="/">Fast Pizza co.</Link>
+      <p>John</p>
+    </header>
+  );
+};
+
+export default Header;
