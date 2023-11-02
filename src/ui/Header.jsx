03b671adb2485cdce3ebf61a19c3c2b1cfa3a8c1
jamesAkash <akash.james@outlook.com>
2023-11-02 19:22:03 +0530

    order loaders complete

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 const Header = () => {
   return (
     <header>
       <Link to="/">Fast Pizza co.</Link>
+      <SearchOrder />
       <p>John</p>
     </header>
   );
